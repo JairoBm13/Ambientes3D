7cb439570f0ceefa8ca6093a29edcb4be10d2c07
JairoBm13 <j.bautista.m13@outlook.com>
2017-03-14 19:18:14 -0500

    primera prueba con tobii for Unity, e inicio de proyecto

---
+++ TobiiProject/Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	private Rigidbody cuerpoRigido;
+	// Use this for initialization
+	void Start () {
+		cuerpoRigido = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movimiento = new Vector3(moveHorizontal, 0f, moveVertical);
+		transform.Translate (movimiento * Time.deltaTime*50);
+	}
+
+	void FixedUpdate()
+	{
+		
+
+	
+	}
+}
